960fda38b887771ab2086563579e952eafec378e
Jas-Sidhu23 <jaskaransidhu@outlook.com>
2025-05-22 20:58:19 -0400

    Initial commit with Spring Boot backend scaffold

---
+++ backend/inventory/src/test/java/com/jaskaran/inventory/InventoryManagerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.jaskaran.inventory;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class InventoryManagerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
